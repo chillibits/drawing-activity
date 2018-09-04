0ae31dc18bdf39ce30e0a8f4812483b0d85f116a
mrgames13 <mrgames@outlook.de>
2018-09-04 15:00:15 +0200

    Initial Commit

---
+++ drawingactivity/src/main/java/com/mrgames13/jimdo/drawingactivity/TestActivity.java
@@ -0,0 +1,13 @@
+package com.mrgames13.jimdo.drawingactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
