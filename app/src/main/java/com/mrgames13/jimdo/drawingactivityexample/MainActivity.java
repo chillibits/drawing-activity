6420d4031ea4c8b77d5dd3afe7a90dfefff333d4
mrgames13 <mrgames@outlook.de>
2018-09-04 15:12:32 +0200

    Initial Commit

--- app/src/main/java/com/mrgames13/jimdo/drawingactivityexample/MainActivity.java
+++ app/src/main/java/com/mrgames13/jimdo/drawingactivityexample/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mrgames13.jimdo.drawingactivityexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.mrgames13.jimdo.drawingactivity.DrawingActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         open_drawing_activity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(MainActivity.this, DrawingActivity.class));
             }
         });
     }
