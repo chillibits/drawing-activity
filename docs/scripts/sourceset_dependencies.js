6dcad1ef660f8792541775ff5af840cd9ee84294
marcauberer <marcauberer@users.noreply.github.com>
2021-03-17 21:14:16 +0000

    Deploying to gh-pages from  @ e47b5e36be912f1d85eccfb2ea3650a9b2b228b8 🚀

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":drawingactivity:dokkaHtml/androidTestRelease": [], ":drawingactivity:dokkaHtml/debug": [], ":drawingactivity:dokkaHtml/main": [], ":drawingactivity:dokkaHtml/release": []}'
+sourceset_dependencies='{":drawingactivity:dokkaHtml/androidTestRelease":[],":drawingactivity:dokkaHtml/debug":[],":drawingactivity:dokkaHtml/main":[],":drawingactivity:dokkaHtml/release":[]}'
